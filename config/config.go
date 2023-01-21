c864ef646011620a48efe5da9cd6f2efe3ba9fb0
kave haj <kave.haj@snapp.cab>
2023-01-22 01:27:22 +0330

    added config func

--- config/config.go
+++ config/config.go
@@ -1,1 +1,22 @@
 package config
+
+import "os"
+
+type Config struct {
+	Server `yaml:"server"`
+}
+
+type Server struct {
+	Port int `yaml:"port"`
+}
+
+func GetConfig() error {
+	//open env file and read config
+	file, err := os.Open("/env.yml")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	return nil
+}
