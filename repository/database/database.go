ee88a3673e0d72a4f93af343fd0ed957d48d2956
kave haj <kave.haj@snapp.cab>
2023-03-03 18:53:20 +0330

    impeliment db interface

--- repository/database/database.go
+++ repository/database/database.go
@@ -1,22 +1,22 @@
 package database
 
 import (
-	"context"
 	"database/sql"
+	"time"
 )
 
 const (
 	//TODO: basedata and details
 
-	InsertProductsMaxIdQuery = "INSERT INTO products (p_id, created_at) VALUES(?, ?);"
-	GetProductsMaxIdQuery    = "SELECT Max(p_id) FROM products LIMIT 1;"
+	InsertProductMaxIdQuery = "INSERT INTO products (p_id, created_at) VALUES(?, ?);"
+	GetProductMaxIdQuery    = "SELECT Max(p_id) FROM products LIMIT 1;"
 
-	InsertCustomersMaxIdQuery = "INSERT INTO customers (c_id, created_at) VALUES(?, ?);"
-	GetCustomersMaxIdQuery    = "SELECT Max(c_id) FROM customers LIMIT 1;"
+	InsertCustomerMaxIdQuery = "INSERT INTO customers (c_id, created_at) VALUES(?, ?);"
+	GetCustomerMaxIdQuery    = "SELECT Max(c_id) FROM customers LIMIT 1;"
 
 	//TODO: invoice id is unique?
-	InsertInvoicesMaxIdQuery = "INSERT INTO invoices (i_id, created_at) VALUES(?, ?);"
-	GetInvoicesMaxIdQuery    = "SELECT Max(i_id) FROM invoices LIMIT 1;"
+	InsertInvoiceMaxIdQuery = "INSERT INTO invoices (i_id, created_at) VALUES(?, ?);"
+	GetInvoiceMaxIdQuery    = "SELECT Max(i_id) FROM invoices LIMIT 1;"
 
 	//TODO: invoice id is unique?
 	InsertTreasuriesMaxIdQuery = "INSERT INTO treasuries (t_id, created_at) VALUES(?, ?);"
@@ -28,26 +28,25 @@ const (
 )
 
 type DatabaseInterface interface {
-	InsertProduct(ctx context.Context, p_id int) error
-	GetProduct(ctx context.Context, p_id int) (int, error)
+	InsertProduct(p_id int) error
+	GetProduct(p_id int) (int, error)
 
-	InsertCustomer(ctx context.Context, c_id int) error
-	GetCustomer(ctx context.Context, c_id int) (int, error)
+	InsertCustomer(c_id int) error
+	GetCustomer(c_id int) (int, error)
 
-	InsertInvoice(ctx context.Context, i_id int) error
-	GetInvoice(ctx context.Context, i_id int) (int, error)
+	InsertInvoice(i_id int) error
+	GetInvoice(i_id int) (int, error)
 
-	InsertTreasuries(ctx context.Context, t_id int) error
-	GetTreasuries(ctx context.Context, t_id int) (int, error)
+	InsertTreasuries(t_id int) error
+	GetTreasuries(t_id int) (int, error)
 
-	InsertReverted(ctx context.Context, r_id int) error
-	GetReverted(ctx context.Context, r_id int) (int, error)
+	InsertReverted(r_id int) error
+	GetReverted(r_id int) (int, error)
 }
 
 type Database struct {
 	sdb *sql.DB
 }
-
 
 func NewDatabase(sdb *sql.DB) DatabaseInterface {
 	return &Database{
@@ -55,53 +54,70 @@ func NewDatabase(sdb *sql.DB) DatabaseInterface {
 	}
 }
 
-
 // GetCustomer implements DatabaseInterface
-func (d *Database) GetCustomer(ctx context.Context, c_id int) (int, error) {
-	panic("unimplemented")
+func (d *Database) GetCustomer(c_id int) (int, error) {
+	err := d.sdb.QueryRow(GetCustomerMaxIdQuery, c_id).Scan(c_id)
+	if err != nil {
+		return 0, err
+	}
+	return c_id, err
 }
 
 // GetInvoice implements DatabaseInterface
-func (d *Database) GetInvoice(ctx context.Context, i_id int) (int, error) {
+func (d *Database) GetInvoice(i_id int) (int, error) {
 	panic("unimplemented")
 }
 
 // GetProduct implements DatabaseInterface
-func (d *Database) GetProduct(ctx context.Context, p_id int) (int, error) {
-	panic("unimplemented")
+func (d *Database) GetProduct(p_id int) (int, error) {
+	err := d.sdb.QueryRow(GetProductMaxIdQuery, p_id).Scan(p_id)
+	if err != nil {
+		return 0, err
+	}
+	return p_id, err
 }
 
 // GetReverted implements DatabaseInterface
-func (d *Database) GetReverted(ctx context.Context, r_id int) (int, error) {
+func (d *Database) GetReverted(r_id int) (int, error) {
 	panic("unimplemented")
 }
 
 // GetTreasuries implements DatabaseInterface
-func (d *Database) GetTreasuries(ctx context.Context, t_id int) (int, error) {
+func (d *Database) GetTreasuries(t_id int) (int, error) {
 	panic("unimplemented")
 }
 
 // InsertCustomer implements DatabaseInterface
-func (d *Database) InsertCustomer(ctx context.Context, c_id int) error {
-	panic("unimplemented")
+func (d *Database) InsertCustomer(c_id int) error {
+	_, err := d.sdb.Exec(InsertCustomerMaxIdQuery, c_id, time.Now())
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // InsertInvoice implements DatabaseInterface
-func (d *Database) InsertInvoice(ctx context.Context, i_id int) error {
+func (d *Database) InsertInvoice(i_id int) error {
 	panic("unimplemented")
 }
 
 // InsertProduct implements DatabaseInterface
-func (d *Database) InsertProduct(ctx context.Context, p_id int) error {
-	panic("unimplemented")
+func (d *Database) InsertProduct(p_id int) error {
+	_, err := d.sdb.Exec(InsertProductMaxIdQuery, p_id)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // InsertReverted implements DatabaseInterface
-func (d *Database) InsertReverted(ctx context.Context, r_id int) error {
+func (d *Database) InsertReverted(r_id int) error {
 	panic("unimplemented")
 }
 
 // InsertTreasuries implements DatabaseInterface
-func (d *Database) InsertTreasuries(ctx context.Context, t_id int) error {
+func (d *Database) InsertTreasuries(t_id int) error {
 	panic("unimplemented")
 }
