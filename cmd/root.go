bb32c5b61ef4a7df1d25fccc42c888c03147560c
kave haj <kave.haj@snapp.cab>
2023-03-03 19:25:17 +0330

    added root cmcd

---
+++ cmd/root.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var configPath string
+
+var rootCMD = &cobra.Command{
+	Use:   "erp-job",
+	Short: "ETL between erps service!",
+}
+
+func init() {
+	rootCMD.PersistentFlags().StringVarP(&configPath, "config-path", "c", "env.yaml", "path to config directory")
+
+}
+
+func Execute() {
+	if err := rootCMD.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
