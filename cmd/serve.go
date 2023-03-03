49576a900c23f1f1d970ddf163495008606e3c83
kave haj <kave.haj@snapp.cab>
2023-03-03 19:27:43 +0330

    added serve

---
+++ cmd/serve.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"erp-job/config"
+	"erp-job/models/fararavand"
+	"erp-job/repository"
+
+	"github.com/labstack/echo/v4"
+	"github.com/spf13/cobra"
+)
+
+// serveCmd represents the serve command
+var serveCmd = &cobra.Command{
+	Use:   "serve",
+	Short: "Serves the url shortner service",
+	Run: func(cmd *cobra.Command, args []string) {
+		serve()
+	},
+}
+
+func serve() {
+	dbs := config.LoadConfig(configPath)
+
+	repos := repository.NewRepository(dbs.SqlitConnection)
+	lgcs := fararavand.NewLogics(repos)
+
+	e := echo.New()
+	e.HideBanner = false
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func init() {
+	rootCMD.AddCommand(serveCmd)
+}
