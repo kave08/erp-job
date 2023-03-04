a5bce4a6315d0bb31b96c76802871431635ea978
kave haj <kave.haj@snapp.cab>
2023-03-04 14:02:12 +0330

    update query and implements all func

--- repository/database/database.go
+++ repository/database/database.go
@@ -9,39 +9,39 @@ const (
 	//TODO: basedata and details
 
 	InsertProductMaxIdQuery = "INSERT INTO products (p_id, created_at) VALUES(?, ?);"
-	GetProductMaxIdQuery    = "SELECT Max(p_id) FROM products LIMIT 1;"
+	GetProductMaxIdQuery    = "SELECT Max(p_id) FROM products;"
 
 	InsertCustomerMaxIdQuery = "INSERT INTO customers (c_id, created_at) VALUES(?, ?);"
-	GetCustomerMaxIdQuery    = "SELECT Max(c_id) FROM customers LIMIT 1;"
+	GetCustomerMaxIdQuery    = "SELECT Max(c_id) FROM customers;"
 
 	//TODO: invoice id is unique?
 	InsertInvoiceMaxIdQuery = "INSERT INTO invoices (i_id, created_at) VALUES(?, ?);"
-	GetInvoiceMaxIdQuery    = "SELECT Max(i_id) FROM invoices LIMIT 1;"
+	GetInvoiceMaxIdQuery    = "SELECT Max(i_id) FROM invoices;"
 
 	//TODO: invoice id is unique?
 	InsertTreasuriesMaxIdQuery = "INSERT INTO treasuries (t_id, created_at) VALUES(?, ?);"
-	GetTreasuriesMaxIdQuery    = "SELECT Max(t_id) FROM treasuries LIMIT 1;"
+	GetTreasuriesMaxIdQuery    = "SELECT Max(t_id) FROM treasuries;"
 
 	//TODO: invoice id is unique?
 	InsertRevertedMaxIdQuery = "INSERT INTO reverted (r_id, created_at) VALUES(?, ?);"
-	GetRevertedMaxIdQuery    = "SELECT Max(r_id) FROM reverted LIMIT 1;"
+	GetRevertedMaxIdQuery    = "SELECT Max(r_id) FROM reverted;"
 )
 
 type DatabaseInterface interface {
 	InsertProduct(p_id int) error
-	GetProduct() (p_id int, err error)
+	GetProduct() (int, error)
 
 	InsertCustomer(c_id int) error
-	GetCustomer() (c_id int, err error)
+	GetCustomer() (int, error)
 
 	InsertInvoice(i_id int) error
-	GetInvoice() (i_id int, err error)
+	GetInvoice() (int, error)
 
 	InsertTreasuries(t_id int) error
-	GetTreasuries() (t_id int, err error)
+	GetTreasuries() (int, error)
 
 	InsertReverted(r_id int) error
-	GetReverted() (r_id int, err error)
+	GetReverted() (int, error)
 }
 
 type Database struct {
@@ -55,36 +55,58 @@ func NewDatabase(sdb *sql.DB) DatabaseInterface {
 }
 
 // GetCustomer implements DatabaseInterface
-func (d *Database) GetCustomer() (c_id int, err error) {
-	err = d.sdb.QueryRow(GetCustomerMaxIdQuery).Err()
+func (d *Database) GetCustomer() (int, error) {
+	var maxId int
+	err := d.sdb.QueryRow(GetCustomerMaxIdQuery).Scan(&maxId)
 	if err != nil {
 		return 0, err
 	}
-	return c_id, err
+
+	return maxId, err
 }
 
 // GetInvoice implements DatabaseInterface
 func (d *Database) GetInvoice() (i_id int, err error) {
-	panic("unimplemented")
-}
-
-// GetProduct implements DatabaseInterface
-func (d *Database) GetProduct() (p_id int, err error) {
-	err = d.sdb.QueryRow(GetProductMaxIdQuery).Err()
+	var maxId int
+	err = d.sdb.QueryRow(GetInvoiceMaxIdQuery).Scan(&maxId)
 	if err != nil {
 		return 0, err
 	}
-	return p_id, err
+
+	return maxId, err
+}
+
+// GetProduct implements DatabaseInterface
+func (d *Database) GetProduct() (int, error) {
+	var maxId int
+	err := d.sdb.QueryRow(GetProductMaxIdQuery).Scan(&maxId)
+	if err != nil {
+		return 0, err
+	}
+
+	return maxId, err
 }
 
 // GetReverted implements DatabaseInterface
-func (d *Database) GetReverted() (r_id int, err error) {
-	panic("unimplemented")
+func (d *Database) GetReverted() (int, error) {
+	var maxId int
+	err := d.sdb.QueryRow(GetRevertedMaxIdQuery).Scan(&maxId)
+	if err != nil {
+		return 0, err
+	}
+
+	return maxId, err
 }
 
 // GetTreasuries implements DatabaseInterface
-func (d *Database) GetTreasuries() (t_id int, err error) {
-	panic("unimplemented")
+func (d *Database) GetTreasuries() (int, error) {
+	var maxId int
+	err := d.sdb.QueryRow(GetTreasuriesMaxIdQuery).Scan(&maxId)
+	if err != nil {
+		return 0, err
+	}
+
+	return maxId, err
 }
 
 // InsertCustomer implements DatabaseInterface
@@ -99,7 +121,12 @@ func (d *Database) InsertCustomer(c_id int) error {
 
 // InsertInvoice implements DatabaseInterface
 func (d *Database) InsertInvoice(i_id int) error {
-	panic("unimplemented")
+	_, err := d.sdb.Exec(InsertInvoiceMaxIdQuery, i_id)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // InsertProduct implements DatabaseInterface
@@ -114,10 +141,20 @@ func (d *Database) InsertProduct(p_id int) error {
 
 // InsertReverted implements DatabaseInterface
 func (d *Database) InsertReverted(r_id int) error {
-	panic("unimplemented")
+	_, err := d.sdb.Exec(InsertRevertedMaxIdQuery, r_id)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // InsertTreasuries implements DatabaseInterface
 func (d *Database) InsertTreasuries(t_id int) error {
-	panic("unimplemented")
+	_, err := d.sdb.Exec(InsertTreasuriesMaxIdQuery, t_id)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
