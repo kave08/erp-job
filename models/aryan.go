f717f704b6e55cd37796d20f1ab995ad994ff834
kave haj <kave.haj@snapp.cab>
2023-09-17 18:42:05 +0330

    update destenation models

---
+++ models/aryan.go
@@ -0,0 +1,112 @@
+package models
+
+import "time"
+
+type AryanSaleOrder struct {
+	CustomerId       int           `json:"customer_id"`
+	VoucherDate      time.Duration `json:"voucher_date"`
+	SecondNumber     int           `json:"second_number"`
+	VoucherDesc      string        `json:"voucher_desc"`
+	StockID          int           `json:"stock_id"`
+	SaleTypeId       int           `json:"sale_type_id"`
+	DeliveryCenterID int           `json:"delivery_center_id"`
+	SaleCenterID     int           `json:"sale_center_id"`
+	PaymentWayID     int           `json:"payment_way_id"`
+	SellerVisitorID  int           `json:"seller_visitor_id"`
+	ServiceGoodsID   int           `json:"service_goods_id"`
+	Quantity         float64       `json:"quantity"`
+	Fee              float64       `json:"fee"`
+	DetailDesc       string        `json:"detail_desc"`
+}
+
+type AryanSaleCustomer struct {
+	CustomerID   int    `json:"customer_id"`
+	CustomerCode string `json:"customer_code"`
+}
+
+type AryanSaleTypeSelect struct {
+	BuySaleTypeID   int    `json:"buy_sale_type_id"`
+	BuySaleTypeCode string `json:"buy_sale_type_code"`
+	BuySaleTypeDesc string `json:"buy_sale_type_desc"`
+}
+
+type AryanSaleCenter4SaleSelect struct {
+	StockID   int    `json:"stock_id"`
+	StockCode string `json:"stock_code"`
+	StockDesc string `json:"stock_desc"`
+}
+
+type AryanSalePaymentSelect struct {
+	PaymentWayID   int    `json:"payment_way_id"`
+	PaymentwayDesc string `json:"payment_way_desc"`
+}
+
+type AryanSaleCenterSelect struct {
+	CentersID   int    `json:"centers_id"`
+	CentersCode string `json:"centers_code"`
+	CenterDesc  string `json:"center_desc"`
+}
+
+type AryanDeliverCenter_SaleSelect struct {
+	CentersID   int    `json:"centers_id"`
+	CentersCode string `json:"centers_code"`
+}
+
+type AryanSalerSelect struct {
+	SaleVisitorID   int    `json:"sale_visitor_id"`
+	SaleVisitorDesc string `json:"sale_visitor_desc"`
+}
+
+type AryanSaleSellerVisitor struct {
+	CentersID   int    `json:"centers_id"`
+	CentersCode string `json:"centers_code"`
+}
+
+type AryanGoods struct {
+	ServiceGoodsID   int    `json:"service_goods_id"`
+	ServiceGoodsCode string `json:"service_goods_code"`
+	ServiceGoodsDesc string `json:"service_goods_desc"`
+	GroupId          int    `json:"group_id"`
+	TypeID           int    `json:"type_id"`
+	SecUnitType      int    `json:"sec_unit_type"`
+	Level1           int    `json:"level1"`
+	Level2           int    `json:"level2"`
+	Level3           int    `json:"level3"`
+}
+
+type AryanSaleProforma struct {
+	CustomerId       int           `json:"customer_id"`
+	VoucherDate      time.Duration `json:"voucher_date"`
+	SecondNumber     string        `json:"second_number"`
+	VoucherDesc      string        `json:"voucher_desc"`
+	StockID          int           `json:"stock_id"`
+	SaleTypeId       int           `json:"sale_type_id"`
+	DeliveryCenterID int           `json:"delivery_center_id"`
+	SaleCenterID     int           `json:"sale_center_id"`
+	PaymentWayID     int           `json:"payment_way_id"`
+	SellerVisitorID  int           `json:"seller_visitor_id"`
+	ServiceGoodsID   int           `json:"service_goods_id"`
+	Quantity         float64       `json:"quantity"`
+	Fee              float64       `json:"fee"`
+	DetailDesc       string        `json:"detail_desc"`
+}
+
+type AryanSaleFactor struct {
+	CustomerId       int           `json:"customer_id"`
+	VoucherDate      time.Duration `json:"voucher_date"`
+	SecondNumber     string        `json:"second_number"`
+	VoucherDesc      string        `json:"voucher_desc"`
+	StockID          int           `json:"stock_id"`
+	SaleTypeId       int           `json:"sale_type_id"`
+	DeliveryCenterID int           `json:"delivery_center_id"`
+	SaleCenterID     int           `json:"sale_center_id"`
+	PaymentWayID     int           `json:"payment_way_id"`
+	SellerID         int           `json:"seller_id"`
+	SaleManID        int           `json:"sale_man_id"`
+	DistributerId    int           `json:"distributer_id"`
+	ServiceGoodsID   int           `json:"service_goods_id"`
+	Quantity         float64       `json:"quantity"`
+	Fee              float64       `json:"fee"`
+	DetailDesc       string        `json:"detail_desc"`
+	Element          float64       `json:"element"`
+}
