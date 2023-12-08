65f6e61a2b4ca31a5465d523e65c4bee3c4e611a
Kave Haj <kave.haj@snapp.cab>
2023-12-09 00:33:05 +0330

    update aryan logics

--- logics/aryan.go
+++ logics/aryan.go
@@ -12,8 +12,9 @@ import (
 )
 
 type AryanInterface interface {
-	PostProductsToSaleFactor(fp []models.Products) (*resty.Response, error)
-	PostSaleCustomer(f models.Fararavand) (*resty.Response, error)
+	PostInoviceToSaleFactor(fp []models.Invoices) (*resty.Response, error)
+	PostCustomerToSaleCustomer(fc []models.Customers) (*resty.Response, error)
+	PostInvoiceToGoods(fp []models.Invoices) (*resty.Response, error)
 }
 
 type Aryan struct {
@@ -34,18 +35,18 @@ func NewAryan(repos *repository.Repository) AryanInterface {
 }
 
 // PostSalesOrder Post all sale order data to the secound ERP
-func (a *Aryan) PostProductsToSaleFactor(fp []models.Products) (*resty.Response, error) {
+func (a *Aryan) PostInoviceToSaleFactor(fp []models.Invoices) (*resty.Response, error) {
 	var newSaleFactor []models.SaleFactor
 
 	for _, item := range fp {
 		newSaleFactor = append(newSaleFactor, models.SaleFactor{
-			CustomerId:     item.Customers.CustomerId,
-			ServiceGoodsID: item.Codekala, // ok
-			Quantity:       float64(item.Invoices.ProductCount),
-			Fee:            float64(item.ProductFee),
-			VoucherDesc:    "ETL-Form Fararavand",
-			SecondNumber:   strconv.Itoa(item.Invoices.InvoiceId),
-			// VoucherDate:      strconv.Itoa(item.Invoices.InvoiceDate),
+			CustomerId:       item.CustomerID,
+			ServiceGoodsID:   item.ProductID, // ok
+			Quantity:         float64(item.ProductCount),
+			Fee:              float64(item.ProductFee),
+			VoucherDesc:      "ETL-Form Fararavand",
+			SecondNumber:     strconv.Itoa(item.InvoiceId),
+			VoucherDate:      item.InvoiceDate,
 			StockID:          10000006,
 			SaleTypeId:       10000001,
 			DeliveryCenterID: 10000002,
@@ -69,29 +70,24 @@ func (a *Aryan) PostProductsToSaleFactor(fp []models.Products) (*resty.Response,
 }
 
 // PostSalesOrder Post all sale order data to the secound ERP
-func (a *Aryan) PostCustomersToSaleFactor(fp []models.Customers) (*resty.Response, error) {
-	var newSaleFactor []models.SaleFactor
+func (a *Aryan) PostInvoiceToGoods(fp []models.Invoices) (*resty.Response, error) {
+	var newGoods []models.Goods
 
 	for _, item := range fp {
-		newSaleFactor = append(newSaleFactor, models.SaleFactor{
-			CustomerId:     item.CustomerId,
-			ServiceGoodsID: 0,
-			Quantity:       0,
-			Fee:            0,
-			VoucherDesc:    "ETL-Form Fararavand",
-			SecondNumber:   strconv.Itoa(item.Invoices.InvoiceId),
-			// VoucherDate:      strconv.Itoa(item.Invoices.InvoiceDate),
-			StockID:          10000006,
-			SaleTypeId:       10000001,
-			DeliveryCenterID: 10000002,
-			SaleCenterID:     10000001,
-			PaymentWayID:     10000001,
-			SellerID:         10000002,
-			SaleManID:        10000001,
+		newGoods = append(newGoods, models.Goods{
+			ServiceGoodsID:   item.ProductID,
+			ServiceGoodsCode: "",
+			ServiceGoodsDesc: item.NameKalaFaktor,
+			GroupId:          0,
+			TypeID:           0,
+			SecUnitType:      0,
+			Level1:           0,
+			Level2:           0,
+			Level3:           0,
 		})
 	}
 
-	res, err := a.restyClient.R().SetBody(newSaleFactor).Post("asdasdasdasd")
+	res, err := a.restyClient.R().SetBody(newGoods).Post("asdasdasdasd")
 	if err != nil {
 		return nil, err
 	}
@@ -103,14 +99,13 @@ func (a *Aryan) PostCustomersToSaleFactor(fp []models.Customers) (*resty.Respons
 	return res, nil
 }
 
-// PostSaleCustomer Post all sale customer data to the secound ERP
-func (a *Aryan) PostSaleCustomer(f models.Fararavand) (*resty.Response, error) {
+func (a *Aryan) PostCustomerToSaleCustomer(fc []models.Customers) (*resty.Response, error) {
 	var newSaleCustomer []models.SaleCustomer
 
-	for _, item := range fp {
+	for _, item := range fc {
 		newSaleCustomer = append(newSaleCustomer, models.SaleCustomer{
-			CustomerID:   item.Customers.CustomerId,
-			CustomerCode: strconv.Itoa(item.Customers.CustomerCodePosti),
+			CustomerID:   item.CustomerId,
+			CustomerCode: strconv.Itoa(item.CustomerCodePosti),
 		})
 	}
 
@@ -124,4 +119,28 @@ func (a *Aryan) PostSaleCustomer(f models.Fararavand) (*resty.Response, error) {
 	}
 
 	return res, nil
+}
+
+// PostSaleCustomer Post all sale customer data to the secound ERP
+func (a *Aryan) PostSaleCustomer(f models.Fararavand) (*resty.Response, error) {
+	// var newSaleCustomer []models.SaleCustomer
+
+	// for _, item := range fp {
+	// 	newSaleCustomer = append(newSaleCustomer, models.SaleCustomer{
+	// 		CustomerID:   item.Customers.CustomerId,
+	// 		CustomerCode: strconv.Itoa(item.Customers.CustomerCodePosti),
+	// 	})
+	// }
+
+	// res, err := a.restyClient.R().SetBody(newSaleCustomer).Post("asdasdasdasd")
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	// if res.StatusCode() != http.StatusOK {
+	// 	fmt.Println(res.Body())
+	// }
+
+	return nil, nil
+	// return res, nil
 }
