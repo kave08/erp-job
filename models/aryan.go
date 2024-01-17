760f2b3f596c05ebe8cc9ab861a680e90aa744b1
Kave Haj <kave.haj@snapp.cab>
2024-01-17 16:13:46 +0330

    update struct

--- models/aryan.go
+++ models/aryan.go
@@ -2,34 +2,35 @@ package models
 
 import "time"
 
-// TODO: invoice
 type SaleOrder struct {
-	CustomerId       int           `json:"customer_id"`
-	VoucherDate      time.Duration `json:"voucher_date"`
-	SecondNumber     int           `json:"second_number"`
-	VoucherDesc      string        `json:"voucher_desc"`
-	StockID          int           `json:"stock_id"`
-	SaleTypeId       int           `json:"sale_type_id"`
-	DeliveryCenterID int           `json:"delivery_center_id"`
-	SaleCenterID     int           `json:"sale_center_id"`
-	PaymentWayID     int           `json:"payment_way_id"`
-	SellerVisitorID  int           `json:"seller_visitor_id"`
-	ServiceGoodsID   int           `json:"service_goods_id"`
-	Quantity         float64       `json:"quantity"`
-	Fee              float64       `json:"fee"`
-	DetailDesc       string        `json:"detail_desc"`
+	CustomerId       int     `json:"customer_id"`
+	VoucherDate      string  `json:"voucher_date"`
+	SecondNumber     int     `json:"second_number"`
+	VoucherDesc      string  `json:"voucher_desc"`
+	StockID          int     `json:"stock_id"`
+	SaleTypeId       int     `json:"sale_type_id"`
+	DeliveryCenterID int     `json:"delivery_center_id"`
+	SaleCenterID     int     `json:"sale_center_id"`
+	PaymentWayID     int     `json:"payment_way_id"`
+	SellerVisitorID  int     `json:"seller_visitor_id"`
+	ServiceGoodsID   int     `json:"service_goods_id"`
+	Quantity         float64 `json:"quantity"`
+	Fee              float64 `json:"fee"`
+	DetailDesc       string  `json:"detail_desc"`
 }
 
-// TODO: done
 type SaleCustomer struct {
 	CustomerID   int    `json:"customer_id"`
 	CustomerCode string `json:"customer_code"`
 }
+
+// TODO: todo
 type SaleTypeSelect struct {
 	BuySaleTypeID   int    `json:"buy_sale_type_id"`
 	BuySaleTypeCode string `json:"buy_sale_type_code"`
 	BuySaleTypeDesc string `json:"buy_sale_type_desc"`
 }
+
 type SaleCenter4SaleSelect struct {
 	StockID   int    `json:"stock_id"`
 	StockCode string `json:"stock_code"`
@@ -37,27 +38,31 @@ type SaleCenter4SaleSelect struct {
 }
 type SalePaymentSelect struct {
 	PaymentWayID   int    `json:"payment_way_id"`
-	PaymentwayDesc string `json:"payment_way_desc"`
+	PaymentwayDesc string `json:"payment_way_desc"` //TODO:added by mohamad
 }
+
+// TODO:
 type SaleCenterSelect struct {
 	CentersID   int    `json:"centers_id"`
-	CentersCode string `json:"centers_code"`
-	CenterDesc  string `json:"center_desc"`
+	CentersCode string `json:"centers_code"` //TODO: added by mohamad
+	CenterDesc  string `json:"center_desc"`  //TODO: added by mohamad
 }
+
+// TODO: tood
 type DeliverCenter_SaleSelect struct {
-	CentersID   int    `json:"centers_id"`
-	CentersCode string `json:"centers_code"`
+	CentersID   int    `json:"centers_id"`   //TODO: added by mohamad
+	CentersCode string `json:"centers_code"` //TODO: added by mohamad
 }
 type SalerSelect struct {
 	SaleVisitorID   int    `json:"sale_visitor_id"`
 	SaleVisitorDesc string `json:"sale_visitor_desc"`
 }
+
 type SaleSellerVisitor struct {
-	CentersID   int    `json:"centers_id"`
-	CentersCode string `json:"centers_code"`
+	CentersID   int    `json:"centers_id"`   //TODO: added by mohamad
+	CentersCode string `json:"centers_code"` //TODO: added by mohamad
 }
 
-// TODO: done!
 type Goods struct {
 	ServiceGoodsID   int    `json:"service_goods_id"`
 	ServiceGoodsCode string `json:"service_goods_code"`
@@ -73,22 +78,21 @@ type Goods struct {
 // TODO: unknown!
 type SaleProforma struct {
 	CustomerId       int           `json:"customer_id"`
-	VoucherDate      time.Duration `json:"voucher_date"`
-	SecondNumber     string        `json:"second_number"`
-	VoucherDesc      string        `json:"voucher_desc"`
+	VoucherDate      time.Duration `json:"voucher_date"`  // ""
+	SecondNumber     string        `json:"second_number"` //""
+	VoucherDesc      string        `json:"voucher_desc"`  //
 	StockID          int           `json:"stock_id"`
 	SaleTypeId       int           `json:"sale_type_id"`
-	DeliveryCenterID int           `json:"delivery_center_id"`
-	SaleCenterID     int           `json:"sale_center_id"`
-	PaymentWayID     int           `json:"payment_way_id"`
-	SellerVisitorID  int           `json:"seller_visitor_id"`
-	ServiceGoodsID   int           `json:"service_goods_id"`
-	Quantity         float64       `json:"quantity"`
-	Fee              float64       `json:"fee"`
+	DeliveryCenterID int           `json:"delivery_center_id"` //TODO: added by mohamad
+	SaleCenterID     int           `json:"sale_center_id"`     // ""
+	PaymentWayID     int           `json:"payment_way_id"`     //""
+	SellerVisitorID  int           `json:"seller_visitor_id"`  // invoice.VisitorCode
+	ServiceGoodsID   int           `json:"service_goods_id"`   //ProductID
+	Quantity         float64       `json:"quantity"`           //ProductCount
+	Fee              float64       `json:"fee"`                //ProductFee
 	DetailDesc       string        `json:"detail_desc"`
 }
 
-// TODO: done
 type SaleFactor struct {
 	CustomerId       int     `json:"customer_id"`
 	VoucherDate      string  `json:"voucher_date"`
