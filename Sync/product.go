201f26e2cb05a9dd9f6f37db98b9eefd0f8bb579
kave08 <kave.mailbox@gmail.com>
2024-02-05 01:56:43 +0330

    added sync product to handle sync process between to ERP

---
+++ Sync/product.go
@@ -0,0 +1,16 @@
+package sync
+
+import (
+	"erp-job/repository"
+	"erp-job/services/aryan"
+	"erp-job/services/fararavand"
+	"fmt"
+)
+
+func SyncProducts(repos *repository.Repository, fr fararavand.FararavandInterface, ar aryan.AryanInterface) {
+	err := fr.SyncProductsWithGoods()
+	if err != nil {
+		fmt.Println("Load SyncProductsWithGoods encountered an error", err.Error())
+		return
+	}
+}
