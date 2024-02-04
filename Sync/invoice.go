f3dfc004ab32b31650dd07023416fc249a563c44
kave08 <kave.mailbox@gmail.com>
2024-02-05 01:55:36 +0330

    added sync invoice to handle sync process between to ERP

---
+++ Sync/invoice.go
@@ -0,0 +1,43 @@
+package sync
+
+import (
+	"erp-job/repository"
+	"erp-job/services/aryan"
+	"erp-job/services/fararavand"
+	"fmt"
+)
+
+
+func SyncInvoice(repos *repository.Repository, fr fararavand.FararavandInterface, ar aryan.AryanInterface) {
+
+	err := fr.SyncInvoicesWithSaleFactor()
+	if err != nil {
+		fmt.Println("Load SyncInvoicesWithSaleFactor encountered an error", err.Error())
+		return
+	}
+
+	err = fr.SyncInvoicesWithSaleOrder()
+	if err != nil {
+		fmt.Println("Load SyncInvoicesWithSaleOrder encountered an error", err.Error())
+		return
+	}
+
+	err = fr.SyncInvoicesWithSalePayment()
+	if err != nil {
+		fmt.Println("Load SyncInvoicesWithSalePayment encountered an error", err.Error())
+		return
+	}
+
+	err = fr.SyncInvoicesWithSalerSelect()
+	if err != nil {
+		fmt.Println("Load SyncInvoicesWithSalerSelect encountered an error", err.Error())
+		return
+	}
+
+	err = fr.SyncInvoicesWithSaleProforma()
+	if err != nil {
+		fmt.Println("Load SyncInvoicesWithSaleProforma encountered an error", err.Error())
+		return
+	}
+
+}
