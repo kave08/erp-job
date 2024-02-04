4d62b26fbe0c6693516f7f656f235113137e453b
kave08 <kave.mailbox@gmail.com>
2024-02-05 01:27:24 +0330

    update interface

--- services/fararavand/interface.go
+++ services/fararavand/interface.go
@@ -1,16 +1,14 @@
 package fararavand
 
-import "erp-job/models"
-
 type FararavandInterface interface {
-	GetCustomersForSaleCustomer() error
-	GetBaseData() (*models.Fararavand, error)
-	GetInvoicesForSaleFactor() ([]models.Invoices, error)
-	GetInvoicesForSaleOrder() ([]models.Invoices, error)
-	GetInvoicesForSalePayment() ([]models.Invoices, error)
-	GetInvoicesForSalerSelect() ([]models.Invoices, error)
-	GetInvoicesForSaleProforma() ([]models.Invoices, error)
-	GetProductsToGoods() ([]models.Products, error)
-	GetTreasuries() ([]models.Fararavand, error)
-	GetInvoiceReturns() ([]models.Fararavand, error)
+	SyncBaseData() error
+	SyncCustomersWithSaleCustomer() error
+	SyncProductsWithGoods() error
+	SyncInvoicesWithSaleFactor() error
+	SyncInvoicesWithSaleOrder() error
+	SyncInvoicesWithSalePayment() error
+	SyncInvoicesWithSalerSelect() error
+	SyncInvoicesWithSaleProforma() error
+	SyncTreasuries() error
+	SyncInvoiceReturns() error
 }
