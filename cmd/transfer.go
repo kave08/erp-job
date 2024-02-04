9357a8597472fcd696ce817b38d0c48604bcd340
kave08 <kave.mailbox@gmail.com>
2024-02-05 01:56:24 +0330

    update transfer proces

--- cmd/transfer.go
+++ cmd/transfer.go
@@ -5,7 +5,7 @@ import (
 	"erp-job/repository"
 	"erp-job/services/aryan"
 	"erp-job/services/fararavand"
-	"fmt"
+	"erp-job/sync"
 
 	"github.com/spf13/cobra"
 )
@@ -28,60 +28,13 @@ func transfer() {
 	ar := aryan.NewAryan(repos)
 	fr := fararavand.NewFararavand(repos, ar)
 
-	_, err := fr.GetBaseData()
-	if err != nil {
-		fmt.Println("Load GetBaseData encountered an error", err.Error())
-		return
-	}
-	err = fr.SyncProductsWithGoods()
-	if err != nil {
-		fmt.Println("Load SyncProductsWithGoods encountered an error", err.Error())
-		return
-	}
-	err = fr.SyncCustomersWithSaleCustomer()
-	if err != nil {
-		fmt.Println("Load SyncCustomersWithSaleCustomer encountered an error", err.Error())
-		return
-	}
-	err = fr.SyncInvoicesWithSaleFactor()
-	if err != nil {
-		fmt.Println("Load SyncInvoicesWithSaleFactor encountered an error", err.Error())
-		return
-	}
+	sync.BaseData(repos, fr, ar)
+	sync.SyncCustomer(repos, fr, ar)
+	sync.SyncInvoice(repos, fr, ar)
+	sync.InvoiceReturns(repos, fr, ar)
+	sync.SyncProducts(repos, fr, ar)
+	sync.Treasuries(repos, fr, ar)
 
-	err = fr.SyncInvoicesWithSaleOrder()
-	if err != nil {
-		fmt.Println("Load SyncInvoicesWithSaleOrder encountered an error", err.Error())
-		return
-	}
-
-	err = fr.SyncInvoicesWithSalePayment()
-	if err != nil {
-		fmt.Println("Load SyncInvoicesWithSalePayment encountered an error", err.Error())
-		return
-	}
-
-	err = fr.SyncInvoicesWithSalerSelect()
-	if err != nil {
-		fmt.Println("Load SyncInvoicesWithSalerSelect encountered an error", err.Error())
-		return
-	}
-
-	err = fr.SyncInvoicesWithSaleProforma()
-	if err != nil {
-		fmt.Println("Load SyncInvoicesWithSaleProforma encountered an error", err.Error())
-		return
-	}
-
-	_, err = fr.GetTreasuries()
-	if err != nil {
-		fmt.Println("Load GetTreasuries encountered an error", err.Error())
-		return
-	}
-	_, err = fr.GetInvoiceReturns()
-	if err != nil {
-		fmt.Println("Load GetInvoiceReturns encountered an error", err.Error())
-	}
 }
 
 func init() {
