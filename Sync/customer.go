e2260bef616d5435be0e6bdd1a7e468d516d6289
kave08 <kave.mailbox@gmail.com>
2024-02-05 01:54:38 +0330

    added sync customer to handle sync process between to ERP

---
+++ Sync/customer.go
@@ -0,0 +1,18 @@
+package sync
+
+import (
+	"erp-job/repository"
+	"erp-job/services/aryan"
+	"erp-job/services/fararavand"
+	"fmt"
+)
+
+func SyncCustomer(repos *repository.Repository, fr fararavand.FararavandInterface, ar aryan.AryanInterface) {
+
+	err := fr.SyncCustomersWithSaleCustomer()
+	if err != nil {
+		fmt.Println("Load SyncCustomersWithSaleCustomer encountered an error", err.Error())
+		return
+	}
+
+}
