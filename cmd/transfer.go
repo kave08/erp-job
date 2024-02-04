f06caed6cbacd3c6e85aebfba07da7ea91904057
kave08 <kave.mailbox@gmail.com>
2024-02-05 01:26:17 +0330

    update transfer fararavand function

--- cmd/transfer.go
+++ cmd/transfer.go
@@ -33,43 +33,43 @@ func transfer() {
 		fmt.Println("Load GetBaseData encountered an error", err.Error())
 		return
 	}
-	_, err = fr.GetProductsToGoods()
+	err = fr.SyncProductsWithGoods()
 	if err != nil {
-		fmt.Println("Load GetProductsToGoods encountered an error", err.Error())
+		fmt.Println("Load SyncProductsWithGoods encountered an error", err.Error())
 		return
 	}
-	err = fr.GetCustomersForSaleCustomer()
+	err = fr.SyncCustomersWithSaleCustomer()
 	if err != nil {
-		fmt.Println("Load GetCustomersForSaleCustomer encountered an error", err.Error())
+		fmt.Println("Load SyncCustomersWithSaleCustomer encountered an error", err.Error())
 		return
 	}
-	_, err = fr.GetInvoicesForSaleFactor()
+	err = fr.SyncInvoicesWithSaleFactor()
 	if err != nil {
-		fmt.Println("Load GetInvoicesForSaleFactor encountered an error", err.Error())
-		return
-	}
-
-	_, err = fr.GetInvoicesForSaleOrder()
-	if err != nil {
-		fmt.Println("Load GetInvoicesForSaleOrder encountered an error", err.Error())
+		fmt.Println("Load SyncInvoicesWithSaleFactor encountered an error", err.Error())
 		return
 	}
 
-	_, err = fr.GetInvoicesForSalePayment()
+	err = fr.SyncInvoicesWithSaleOrder()
 	if err != nil {
-		fmt.Println("Load GetInvoicesForSalePayment encountered an error", err.Error())
+		fmt.Println("Load SyncInvoicesWithSaleOrder encountered an error", err.Error())
 		return
 	}
 
-	_, err = fr.GetInvoicesForSalerSelect()
+	err = fr.SyncInvoicesWithSalePayment()
 	if err != nil {
-		fmt.Println("Load GetInvoicesForSalerSelect encountered an error", err.Error())
+		fmt.Println("Load SyncInvoicesWithSalePayment encountered an error", err.Error())
 		return
 	}
 
-	_, err = fr.GetInvoicesForSaleProforma()
+	err = fr.SyncInvoicesWithSalerSelect()
 	if err != nil {
-		fmt.Println("Load GetInvoicesForSaleProforma encountered an error", err.Error())
+		fmt.Println("Load SyncInvoicesWithSalerSelect encountered an error", err.Error())
+		return
+	}
+
+	err = fr.SyncInvoicesWithSaleProforma()
+	if err != nil {
+		fmt.Println("Load SyncInvoicesWithSaleProforma encountered an error", err.Error())
 		return
 	}
 
