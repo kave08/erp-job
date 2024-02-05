fa35206238bac0ed462a16857093632b8dffe487
kave08 <kave.mailbox@gmail.com>
2024-02-05 13:38:52 +0330

    update interface

--- services/fararavand/interface.go
+++ services/fararavand/interface.go
@@ -5,7 +5,7 @@ import "erp-job/models"
 type FararavandInterface interface {
 	SyncBaseData() error
 	SyncCustomersWithSaleCustomer(customers []models.Customers) error
-	SyncProductsWithGoods() error
+	SyncProductsWithGoods(newProducts []models.Products) error
 	SyncInvoicesWithSaleFactor(invoices []models.Invoices) error
 	SyncInvoicesWithSaleOrder(invoices []models.Invoices) error
 	SyncInvoicesWithSalePayment(invoices []models.Invoices) error
