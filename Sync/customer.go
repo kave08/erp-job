cd80d682ffc41f2d04c8db6df7bf66a67d33ac31
kave08 <kave.mailbox@gmail.com>
2024-02-05 13:29:28 +0330

    update customer api process

--- Sync/customer.go
+++ Sync/customer.go
@@ -1,18 +1,64 @@
 package sync
 
 import (
+	"erp-job/config"
+	"erp-job/models"
 	"erp-job/repository"
 	"erp-job/services/aryan"
 	"erp-job/services/fararavand"
+	"erp-job/utility"
 	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-resty/resty/v2"
 )
 
-func Customer(repos *repository.Repository, fr fararavand.FararavandInterface, ar aryan.AryanInterface) {
+type Customers struct {
+	restyClient *resty.Client
+	baseURL     string
+	httpClient  *http.Client
+	repos       *repository.Repository
+	aryan       aryan.AryanInterface
+	fararavand  fararavand.FararavandInterface
+}
 
-	err := fr.SyncCustomersWithSaleCustomer()
+func NewCustomers(repos *repository.Repository, fr fararavand.FararavandInterface, ar aryan.AryanInterface, requestTimeout time.Duration) *Customers {
+	c := resty.New().
+		SetHeader("ApiKey", config.Cfg.FararavandApp.APIKey).SetBaseURL(config.Cfg.FararavandApp.BaseURL)
+
+	return &Customers{
+		restyClient: c,
+		baseURL:     config.Cfg.FararavandApp.BaseURL,
+		repos:       repos,
+		aryan:       ar,
+		fararavand:  fr,
+		httpClient: &http.Client{
+			Timeout: requestTimeout,
+		},
+	}
+}
+
+func (c Customers) Customer() error {
+
+	var newCustomers []models.Customers
+
+	resp, err := c.restyClient.R().SetResult(newCustomers).Get(utility.FGetCustomers)
 	if err != nil {
-		fmt.Println("Load SyncCustomersWithSaleCustomer encountered an error", err.Error())
-		return
+		return err
 	}
 
+	if resp.StatusCode() != http.StatusOK {
+		log.Printf("status code: %d", resp.StatusCode())
+		return fmt.Errorf(utility.ErrNotOk)
+	}
+
+	err = c.fararavand.SyncCustomersWithSaleCustomer(newCustomers)
+	if err != nil {
+		fmt.Println("Load SyncCustomersWithSaleCustomer encountered an error", err.Error())
+		return err
+	}
+
+	return nil
 }
