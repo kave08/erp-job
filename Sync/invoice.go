65432ecdda3aa54d43c74f24935c5f02b7eaa7aa
kave08 <kave.mailbox@gmail.com>
2024-02-19 14:36:47 +0330

    remove inject http and handle all invoice request

--- Sync/invoice.go
+++ Sync/invoice.go
@@ -1,47 +1,41 @@
 package sync
 
 import (
-	"context"
+	"encoding/json"
 	"erp-job/config"
 	"erp-job/models"
 	"erp-job/repository"
 	"erp-job/services/aryan"
 	"erp-job/services/fararavand"
+	"erp-job/utility"
+	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 	"time"
-
-	"erp-job/helpers"
-
-	"github.com/labstack/echo/v4"
 )
-
-const (
-	handleGetInvoiceEndpoint = "GetInvoices"
-)
-
-type InvoiceRequest struct {
-	LatestId int `json:"latest_id"`
-	Invoices []models.Invoices
-}
 
 // InvoiceResponse is the response for the invoice
 type InvoiceResponse struct {
-	Status  int    `json:"status"`
-	Message string `json:"message"`
+	Status      int               `json:"status"`
+	NewInvoices []models.Invoices `json:"new_invoice"`
 }
 
-// NewInvoiceResponse is the InvoiceResponse factory method
-func NewInvoiceResponse(status int, message string) *InvoiceResponse {
-	return &InvoiceResponse{
-		Status:  status,
-		Message: message,
+type InvoiceRequest struct {
+	LastId      int `json:"LastId"`
+	PageSize    int `json:"PageSize"`
+	PageNumeber int `json:"PageNumeber"`
+}
+
+// NewInvoiceRequest is the InvoiceResponse factory method
+func NewInvoiceRequest(lastid int, pageSize int, pageNumber int) ProductRequest {
+	return ProductRequest{
+		LastId:      lastid,
+		PageSize:    pageSize,
+		PageNumeber: pageNumber,
 	}
 }
 
 type Invoice struct {
-	e          *echo.Echo
 	baseURL    string
 	httpClient *http.Client
 	repos      *repository.Repository
@@ -50,20 +44,7 @@ type Invoice struct {
 }
 
 func NewInvoice(repos *repository.Repository, fr fararavand.FararavandInterface, ar aryan.AryanInterface, requestTimeout time.Duration) *Invoice {
-	e := echo.New()
-	e.Use(
-		func(next echo.HandlerFunc) echo.HandlerFunc {
-			return func(c echo.Context) error {
-				req := c.Request()
-				req.URL.Scheme = "https"
-				req.URL.Host = config.Cfg.FararavandApp.BaseURL
-				req.Header.Set("ApiKey", config.Cfg.FararavandApp.APIKey)
-				return next(c)
-			}
-		},
-	)
 	return &Invoice{
-		e:          e,
 		baseURL:    config.Cfg.FararavandApp.BaseURL,
 		repos:      repos,
 		aryan:      ar,
@@ -74,94 +55,87 @@ func NewInvoice(repos *repository.Repository, fr fararavand.FararavandInterface,
 	}
 }
 
-func (cl *client) Invoices(ctx context.Context, inoviceId int) func(c echo.Context) error {
-	return func(c echo.Context) error {
+func (i *Invoice) Invoices() error {
 
-		url := helpers.EncodeURLParams(cl.baseURL+handleGetInvoiceEndpoint, map[string]string{
-			"inovice_id": strconv.Itoa(inoviceId),
-		})
+	request := new(InvoiceRequest)
 
-		req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
-		if err != nil {
-			return err
-		}
-
-		response := new(InvoiceResponse)
-
-		err = cl.call(req, response)
-
-		request := new(InvoiceRequest)
-
-		if err := c.Bind(request); err != nil {
-			log.Printf(
-				"@ERP.sync.invoice.Invoices",
-				"message", "bind invoice",
-				"error", err,
-			)
-			return c.JSON(http.StatusBadRequest, NewInvoiceResponse(http.StatusBadRequest, "validation.required"))
-		}
-
-		if request.LatestId <= 0 {
-			log.Printf(
-				"@ERP.sync.invoice.Invoices",
-				"message", "invalid payload",
-			)
-
-			return c.JSON(http.StatusBadRequest, NewInvoiceResponse(http.StatusBadRequest, "validation.required"))
-		}
-
-		// i.e.GET(handleGetInvoiceEndpoint+"/:id", func(c echo.Context) error {
-		// 	id := c.Param("latest_id")
-		// 	return c.String(http.StatusOK, "User ID: "+id)
-		// })
-
-		// if i.e.AcquireContext().Response().Status != http.StatusOK {
-		// 	log.Printf("status code: %d", i.e.AcquireContext().Response().Status)
-		// 	return fmt.Errorf(utility.ErrNotOk)
-		// }
-
-		// err = i.fararavand.SyncInvoicesWithSaleFactor(request.Invoices)
-		// if err != nil {
-		// 	fmt.Println("Load SyncInvoicesWithSaleFactor encountered an error", err.Error())
-		// 	return err
-		// }
-
-		// err = i.fararavand.SyncInvoicesWithSaleOrder(request.Invoices)
-		// if err != nil {
-		// 	fmt.Println("Load SyncInvoicesWithSaleOrder encountered an error", err.Error())
-		// 	return err
-		// }
-
-		// err = i.fararavand.SyncInvoicesWithSalePayment(request.Invoices)
-		// if err != nil {
-		// 	fmt.Println("Load SyncInvoicesWithSalePayment encountered an error", err.Error())
-		// 	return err
-		// }
-
-		// err = i.fararavand.SyncInvoicesWithSalerSelect(request.Invoices)
-		// if err != nil {
-		// 	fmt.Println("Load SyncInvoicesWithSalerSelect encountered an error", err.Error())
-		// 	return err
-		// }
-
-		// err = i.fararavand.SyncInvoicesWithSaleProforma(request.Invoices)
-		// if err != nil {
-		// 	fmt.Println("Load SyncInvoicesWithSaleProforma encountered an error", err.Error())
-		// 	return err
-		// }
-
-		// err = i.fararavand.SyncInvoicesWithSaleCenter(request.Invoices)
-		// if err != nil {
-		// 	fmt.Println("Load SyncInvoicesWithSaleCenter encountered an error", err.Error())
-		// 	return err
-		// }
-
-		// err = i.fararavand.SyncInvoiceWithSaleTypeSelect(request.Invoices)
-		// if err != nil {
-		// 	fmt.Println("Load SyncInvoicesWithSaleCenter encountered an error", err.Error())
-		// 	return err
-		// }
-
-		return nil
+	req, err := http.NewRequest(http.MethodGet, i.baseURL+
+		fmt.Sprintf("/GetInvoices?PageNumeber=%d&PageSize=%d&LastId=%d/", request.PageNumeber, request.PageSize, request.LastId), nil)
+	if err != nil {
+		return err
 	}
+
+	req.Header.Set("ApiKey", config.Cfg.FararavandApp.APIKey)
+
+	res, err := i.httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return fmt.Errorf("get invoice http request failed. status: %d, response: %v", res.StatusCode, res.Body)
+	}
+
+	response := new(InvoiceResponse)
+	err = json.NewDecoder(res.Body).Decode(response)
+	if err != nil {
+		return err
+	}
+
+	if res.StatusCode != response.Status {
+		return fmt.Errorf("driver profile http request failed(body). status: %d, response: %v", response.Status, res.Body)
+	}
+
+	if res.StatusCode != http.StatusOK {
+		log.Printf("status code: %d", res.StatusCode)
+		return fmt.Errorf(utility.ErrNotOk)
+	}
+
+	if request.LastId <= 0 {
+		return fmt.Errorf("validation.required %d", http.StatusBadRequest)
+	}
+
+	err = i.fararavand.SyncInvoicesWithSaleFactor(response.NewInvoices)
+	if err != nil {
+		fmt.Println("load SyncInvoicesWithSaleFactor encountered an error: %w", err)
+		return err
+	}
+
+	err = i.fararavand.SyncInvoicesWithSaleOrder(response.NewInvoices)
+	if err != nil {
+		fmt.Println("load SyncInvoicesWithSaleOrder encountered an error: %w", err)
+		return err
+	}
+
+	err = i.fararavand.SyncInvoicesWithSalePayment(response.NewInvoices)
+	if err != nil {
+		fmt.Println("load SyncInvoicesWithSalePayment encountered an error: %w", err)
+		return err
+	}
+
+	err = i.fararavand.SyncInvoicesWithSalerSelect(response.NewInvoices)
+	if err != nil {
+		fmt.Println("load SyncInvoicesWithSalerSelect encountered an error: %w", err)
+		return err
+	}
+
+	err = i.fararavand.SyncInvoicesWithSaleProforma(response.NewInvoices)
+	if err != nil {
+		fmt.Println("load SyncInvoicesWithSaleProforma encountered an error: %w", err)
+		return err
+	}
+
+	err = i.fararavand.SyncInvoicesWithSaleCenter(response.NewInvoices)
+	if err != nil {
+		fmt.Println("load SyncInvoicesWithSaleCenter encountered an error: %w", err)
+		return err
+	}
+
+	err = i.fararavand.SyncInvoiceWithSaleTypeSelect(response.NewInvoices)
+	if err != nil {
+		fmt.Println("load SyncInvoicesWithSaleCenter encountered an error: %w", err)
+		return err
+	}
+
+	return nil
 }
