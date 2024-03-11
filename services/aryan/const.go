451b9ed88414ec264e400311d54b9777c5cf1adc
kave08 <kave.mailbox@gmail.com>
2024-03-11 14:19:38 +0330

    update const

---
+++ services/aryan/const.go
@@ -0,0 +1,17 @@
+package aryan
+
+const (
+	//aryan const endpoint
+	SaleOrder               = "SaleOrder"
+	SaleCustomer            = "SaleCustomer"
+	SaleTypeSelect          = "SaleTypeSelect"
+	SaleCenter4SaleSelect   = "SaleCenter4SaleSelect"
+	SalePaymentSelect       = "SalePaymentSelect"
+	SaleCenterSelect        = "SaleCenterSelect"
+	DeliverCenterSaleSelect = "DeliverCenter_SaleSelect"
+	SalerSelect             = "SalerSelect"
+	SaleSellerVisitor       = "SaleSellerVisitor"
+	Goods                   = "Goods"
+	SaleProforma            = "SaleProforma"
+	SaleFactor              = "SaleFactor"
+)
