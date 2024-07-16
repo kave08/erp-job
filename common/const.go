ae790f0d97803a1da82847f2626f8c40448edfeb
kave08 <kave.mailbox@gmail.com>
2024-07-16 19:51:30 +0330

    addd api const

---
+++ common/const.go
@@ -0,0 +1,27 @@
+package common
+
+const (
+	GetProducts       = "/GetProducts?PageNumeber=%d&PageSize=%d&LastId=%d/"
+	GetInvoices       = "/GetInvoices?PageNumeber=%d&PageSize=%d&LastId=%d/"
+	GetCustomers      = "/GetCustomers?PageNumeber=%d&PageSize=%d&LastId=%d/"
+	GetBaseData       = "/GetBaseData?PageNumeber=%d&PageSize=%d&LastId=%d/"
+	GetTreasuries     = "/GetTreasuries?PageNumeber=%d&PageSize=%d&LastId=%d/"
+	GetInvoiceReturns = "/GetInvoiceReturns?PageNumeber=%d&PageSize=%d&LastId=%d/"
+)
+
+// const (
+// 	//aryan const endpoint
+// 	SaleOrder               = "SaleOrder"
+// 	SaleCustomer            = "SaleCustomer"
+// 	SaleTypeSelect          = "SaleTypeSelect"
+// 	SaleCenter4SaleSelect   = "SaleCenter4SaleSelect"
+// 	SalePaymentSelect       = "SalePaymentSelect"
+// 	SaleCenterSelect        = "SaleCenterSelect"
+// 	DeliverCenterSaleSelect = "DeliverCenter_SaleSelect"
+// 	SalerSelect             = "SalerSelect"
+// 	SaleSellerVisitor       = "SaleSellerVisitor"
+// 	Goods                   = "Goods"
+// 	SaleProforma            = "SaleProforma"
+// 	SaleFactor              = "SaleFactor"
+// 	Login                   = "Api/Api/login"
+// )
